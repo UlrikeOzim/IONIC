602009610259e45dc39e48eecfb79d9dd524c5b1
UlrikeOzim <ulrike@ozim.at>
2021-01-31 15:55:13 +0100

    added needed components for popover-camera usage

---
+++ src/app/tab1/popover.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1-popover-component',
+  templateUrl: 'popover.component.html',
+  styleUrls: ['popover.component.scss']
+})
+export class PopoverComponent {
+
+  constructor() {}
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from './popover.component';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public popoverController: PopoverController) {}
 
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      cssClass: 'my-custom-class',
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
 }
